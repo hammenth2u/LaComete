f33fae94ddc1c31c20cfed9e40df10bec80bb526
Posh-Posh <pauline_fremont@live.fr>
2019-10-16 12:19:55 +0200

    fix up axios request

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -41,7 +41,7 @@ class App extends React.Component {
   abortController = new AbortController();
 
 
-    getUserStatus = () => {axios.get('http://ec2-3-84-230-242.compute-1.amazonaws.com/api/user/isConnected'), {cancelToken: this.source.token}
+    getUserStatus = () => {axios.get('http://ec2-3-84-230-242.compute-1.amazonaws.com/api/user/isConnected', {cancelToken: this.source.token})
     //axios.get('http://127.0.0.1:8001/api/user/isConnected'
       
       .then(response => {
